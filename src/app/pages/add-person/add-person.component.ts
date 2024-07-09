010bdf9d827e5f107d58405025e93c89ed182341
DayanaVarg <dalejav2295@gmail.com>
2024-07-09 12:32:59 -0500

    FEAT: Project Creation

--- src/app/pages/add-person/add-person.component.ts
+++ src/app/pages/add-person/add-person.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators, FormGroup, FormControl } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -10,21 +10,19 @@ import { RouterModule } from '@angular/router';
   styleUrl: './add-person.component.css'
 })
 export class AddPersonComponent {
+  private fb = inject(FormBuilder);
 
-  form = signal<FormGroup>(
-    new FormGroup(
-    {
-      ide : new FormControl<String>('', Validators.required),
-      name : new FormControl<String>('', Validators.required),
-      lastname:new FormControl<String>('', Validators.required),
-      email:new FormControl<String>('', Validators.required),
-      phone:new FormControl<String>('', Validators.required),
-      dateB:new FormControl<String>('', Validators.required)
-    })
-  );
+  form = this.fb.group({
+    iden : ['' , [Validators.required]],
+    name : ['', [Validators.required]],
+    lastname:['', [Validators.required]],
+    email:['', [Validators.required]],
+    phone:['', [Validators.required]],
+    dateB:['', [Validators.required]]
+  });
 
   create(){
-    console.log(this.form().value);
+    console.log(this.form.value)
   }
   
 }
